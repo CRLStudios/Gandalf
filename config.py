715d3bfc79fe9b80d6d2d2caa034ec6d503f9a00
Cory Leach of Legends <cory.leach@gmail.com>
2026-05-16 22:47:39 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 BOT_TOKEN = os.getenv("DISCORD_TOKEN", "")
-SCRIPT_TIMEOUT = int(os.getenv("SCRIPT_TIMEOUT", "60"))
+SCRIPT_TIMEOUT = int(os.getenv("SCRIPT_TIMEOUT", "600"))
 SCRIPTS_DIR = Path(__file__).parent / "scripts"
 SCHEDULES_DIR = Path(__file__).parent / "schedules"
 SHORTCUTS_DIR = Path(__file__).parent / "shortcuts"
